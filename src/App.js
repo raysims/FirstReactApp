c0de20c3fa1fb65c327d6d965b5e28d8d16323c1
Ray Sims <rsims515@gmail.com>
2018-04-21 16:18:29 -0400

    updating the look

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import CountingButton from "./components/CountingButton.js";
 import Header from "./components/Header.js"
 import Footer from "./components/Footer.js"
 import LinkedInWidget from "./components/LinkedInWidget.js"
-
+import Button from 'material-ui/Button';
+import AppBar from 'material-ui/AppBar';
 
 class App extends Component {
 
@@ -17,15 +18,34 @@ class App extends Component {
       document.body.appendChild(script);
   }
 
+  constructor(props) {
+  super(props);
+  this.state = { counter: 1 };
+  }
+
+  handleClick = () => {
+    console.log('Button is clicked!!');
+    this.setState((prevState) => ({
+      counter: prevState.counter + 1
+    }));
+  };
   render() {
 
     return (
+      <body>
         <div className="App">
-          <Header></Header>
-          <CountingButton className="App"></CountingButton>
-          <LinkedInWidget></LinkedInWidget>
-          <Footer></Footer>
+          <AppBar title="Title" iconClassNameRight="muidocs-icon-navigation-expand-more">
+            <Header />
+          </AppBar>
         </div>
+        <div className="App">
+          <Button variant="raised" color="primary"onClick={this.handleClick}>
+            How many times can you click me? {this.state.counter}</Button>
+          <CountingButton className="App"></CountingButton>
+          <LinkedInWidget />
+          <Footer />
+        </div>
+      </body>
     );
 
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import Button from 'material-ui/Button';
 
 export default class Footer extends Component {
 
+
   render() {
     return (
+
       <footer className="App-footer">The more you know, the more there is to learn
-        <br></br>
-         <a href="https://github.com/raysims">Visit My Github!</a>
+        <br />
+        <Button href='http://github.com/raysims' alt='http://github.com/raysims' variant="raised" color="primary" text="bold">Click Here for my GitHub</Button>
       </footer>
     );
   }
