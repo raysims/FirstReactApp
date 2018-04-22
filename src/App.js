b3ff916e2f6e3e588a5f59b3810b02ffad9b71d2
Ray Sims <rsims515@gmail.com>
2018-04-22 11:47:26 -0400

    Rebranded to become DSC site

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import CountingButton from "./components/CountingButton.js";
 import Header from "./components/Header.js"
 import Footer from "./components/Footer.js"
 import LinkedInWidget from "./components/LinkedInWidget.js"
 import Button from 'material-ui/Button';
 import AppBar from 'material-ui/AppBar';
-import Intro from "./components/Intro.js"
+import Intro from "./components/Intro.js";
+import Subaru from "./subaru.jpg";
 
 class App extends Component {
 
@@ -39,10 +39,14 @@ class App extends Component {
             <Header className="App"/>
           </AppBar>
         </div>
+        <div>
+          <Button variant="raised" color="primary">Meet Our Members</Button>
+          <Button variant="raised" color="primary">Event Calendar</Button>
+          <Button variant="raised" color="primary">Our Cars</Button>
+          <Button variant="raised" color="primary">Shop</Button>
+        </div>
         <div className="App">
-          <Button variant="raised" color="primary"onClick={this.handleClick}>
-            How many times can you click me? {this.state.counter}</Button>
-          <CountingButton className="App"></CountingButton>
+          <img src={require('./subaru.jpg')} />
         </div>
         <div>
           <LinkedInWidget />
