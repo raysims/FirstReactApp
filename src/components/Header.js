dc4928f4e7921cf0405cb2a290ab183feb6623c2
Ray Sims <rsims515@gmail.com>
2018-04-22 11:24:10 -0400

    Adding react router Starting to rebrand

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export default class Header extends Component {
   render() {
     return (
       <div>
-        <h1 className="App-header">Welcome To My Website!</h1>
+        <h1 className="App-header">Delaware Subaru Club</h1>
       </div>
     );
   }
