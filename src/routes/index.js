d28e01466e6d664c4b9c82bc9302f88b70f856e2
Ray Sims <rsims515@gmail.com>
2018-04-22 14:41:45 -0400

    initial routing set up

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,18 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import Home from "./Home.js";
+import Events from "./Events.js";
+import MeetMembers from "./MeetMembers";
+import MemberCars from "./MemberCars";
 
 export default () => (
   <BrowserRouter>
-    <Route path="/" component={Home} />
+    <div>
+      <Route path="/" exact component={Home} />
+      <Route path="/home" exact component={Home} />
+      <Route path="/events" exact component={Events} />
+      <Route path="/meetmembers" exact component={MeetMembers} />
+      <Route path="/membercars" exact component={MemberCars} />
+    </div>
   </BrowserRouter>
 );
